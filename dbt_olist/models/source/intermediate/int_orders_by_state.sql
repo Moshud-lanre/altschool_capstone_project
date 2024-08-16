743599e4efb98181b5980d8a087a3158ca198785
Moshud-lanre <lanremoshud@gmail.com>
2024-08-16 18:06:11 +0100

    DBT: Setup and running

---
+++ dbt_olist/models/source/intermediate/int_orders_by_state.sql
@@ -0,0 +1,21 @@
+WITH order_state AS(
+    SELECT
+        o.customer_id,
+        c.state,
+        order_id
+    FROM 
+        {{ ref('stg_orders') }} o
+    JOIN
+        {{ source('raw', 'customers') }} c 
+    ON
+        o.customer_id = c.customer_id
+
+)
+
+SELECT 
+    state,
+    count(order_id) AS num_of_orders
+FROM
+    order_state
+GROUP BY
+    state
